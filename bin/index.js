d955aae35141ff5d2721f88cbd55f70c136032e4
sho ochiai <ochiai_s@welmo.co.jp>
2025-02-25 22:10:34 +0900

    feat: use TypeScript

--- bin/index.js
+++ bin/index.js
@@ -8,10 +8,10 @@ import { dirname } from "path";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-// 1. Create base React project with Vite
+// 1. Create base React + TypeScript project with Vite
 const projectName = process.argv[2] || "my-react-app";
 console.log(`🚀 Creating a new React app in ${projectName}...`);
-execSync(`npm create vite@latest ${projectName} -- --template react`, { stdio: "inherit" });
+execSync(`npm create vite@latest ${projectName} -- --template react-ts`, { stdio: "inherit" });
 
 // 2. Install dependencies
 console.log("📦 Installing dependencies...");
